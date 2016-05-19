af8eedf5b207c4b43bf7f88d736a181cc69d95af
PANGJIANLIN <PANGJIANLIN@kingsoft.com>
2016-05-19 16:19:51 +0800

    a little change

--- src/main/java/com/iproject/maven/dao/StuDAOImpl.java
+++ src/main/java/com/iproject/maven/dao/StuDAOImpl.java
@@ -2,9 +2,7 @@ package com.iproject.maven.dao;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Repository;
-
 import com.iproject.maven.model.StuVO;
 
 @Repository
@@ -16,23 +14,22 @@ public class StuDAOImpl implements StuDAO{
 		try {
 			StuVO stu1 = new StuVO();
 			stu1.setId(001);
-			stu1.setName("小明");
+            stu1.setName("小明");
 			stu1.setScore(99);
 			stu.add(stu1);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		StuVO stu2 = new StuVO();
 		stu2.setId(002);
-		stu2.setName("小王");
+        stu2.setName("小王");
 		stu2.setScore(55);
 		stu.add(stu2);
 		
 		StuVO stu3 = new StuVO();
 		stu3.setId(003);
-		stu3.setName("小李");
+        stu3.setName("小李");
 		stu3.setScore(92);
 		stu.add(stu3);
 		
